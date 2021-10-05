e7d00c2c7b3648f2ee196eccb4b7d1b417a888ab
HIRCHAUD Edouard <edouard.hirchaud@anses.fr>
2021-10-05 09:05:34 +0200

    first commit for ansible

---
+++ galaxy/files/proxy/proxy.sh
@@ -0,0 +1,12 @@
+i# set proxy config via profie.d - should apply for all users
+# 
+export http_proxy="http://alfortproxy.afssa.fr:8080/"
+export https_proxy="http://alfortproxy.afssa.fr:8080/"
+export ftp_proxy="http://alfortproxy.afssa.fr:8080/"
+export no_proxy="127.0.0.1,localhost"
+
+# For curl
+export HTTP_PROXY="http://alfortproxy.afssa.fr:8080/"
+export HTTPS_PROXY="http://alfortproxy.afssa.fr:8080/"
+export FTP_PROXY="http://alfortproxy.afssa.fr:8080/"
+export NO_PROXY="127.0.0.1,localhost"
