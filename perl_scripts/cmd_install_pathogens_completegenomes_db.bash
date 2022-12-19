28be54ae1980cc52eff2c9c30b7a0cf3a72f4620
f.touzain <fabrice.touzain@anses.fr>
2022-12-19 11:01:23 +0100

    modify db dir rights to allow use by Galaxy

--- perl_scripts/cmd_install_pathogens_completegenomes_db.bash
+++ perl_scripts/cmd_install_pathogens_completegenomes_db.bash
@@ -13,5 +13,8 @@ sudo chown pmv:galaxy $bactdb
 perl -r ../taxid_lists/README.md -o $virdb -keep_taxid
 perl -r ../taxid_lists/README.md -o $bactdb -keep_taxid -b
 
+sudo chown -R galaxy:galaxy $virdb
+sudo chown -R galaxy:galaxy $bactdb
+
 # then need to add path "${virdb}rvcg_blast_db" to blast tool in Galaxy
 # then need to add path "${bactdb}rbcg_blast_db" to blast tool in Galaxy
