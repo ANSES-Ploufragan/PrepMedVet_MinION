0fb2cc84e0d33926789112a321d207d6e036a0a7
f.touzain <fabrice.touzain@anses.fr>
2023-02-14 14:59:17 +0100

    feat(tools): change to variable ip and api-key in script for install tools and workshop

--- galaxy_workflow/install_1/workflows-to-tools.sh
+++ galaxy_workflow/install_1/workflows-to-tools.sh
@@ -1,3 +1,5 @@
+export ip=$1
+export api=$2
 workflow-to-tools -w Galaxy-Workflow-Catch_clean_gi_to_DL_ref.ga                        -o Galaxy-Workflow-Catch_clean_gi_to_DL_ref.yml
 workflow-to-tools -w Galaxy-Workflow-V1__Alignment___Variant_calling.ga                 -o Galaxy-Workflow-V1__Alignment___Variant_calling.yml
 workflow-to-tools -w Galaxy-Workflow-V1__Concatenate_reads_minion_files.ga              -o Galaxy-Workflow-V1__Concatenate_reads_minion_files.yml
@@ -5,17 +7,17 @@ workflow-to-tools -w Galaxy-Workflow-V1__Megablast_virus_prok_from_reads.ga     
 workflow-to-tools -w Galaxy-Workflow-V2__Alignment___Get_unmapped_reads.ga              -o Galaxy-Workflow-V2__Alignment___Get_unmapped_reads.yml
 workflow-to-tools -w Galaxy-Workflow-V3___alignment___assembly___megablast___tblastx.ga -o Galaxy-Workflow-V3___alignment___assembly___megablast___tblastx.yml
 workflow-to-tools -w Galaxy-Workflow-V3___concatenate_megablast_results_if_needed.ga    -o Galaxy-Workflow-V3___concatenate_megablast_results_if_needed.yml
-shed-tools install -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb -t Galaxy-Workflow-Catch_clean_gi_to_DL_ref.yml
-shed-tools install -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb -t Galaxy-Workflow-V1__Alignment___Variant_calling.yml
-shed-tools install -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb -t Galaxy-Workflow-V1__Concatenate_reads_minion_files.yml
-shed-tools install -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb -t Galaxy-Workflow-V1__Megablast_virus_prok_from_reads.yml
-shed-tools install -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb -t Galaxy-Workflow-V2__Alignment___Get_unmapped_reads.yml
-shed-tools install -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb -t Galaxy-Workflow-V3___alignment___assembly___megablast___tblastx.yml
-shed-tools install -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb -t Galaxy-Workflow-V3___concatenate_megablast_results_if_needed.yml
-workflow-install  -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb --publish_workflows -w Galaxy-Workflow-Catch_clean_gi_to_DL_ref.ga
-workflow-install  -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb --publish_workflows -w Galaxy-Workflow-V1__Alignment___Variant_calling.ga
-workflow-install  -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb --publish_workflows -w Galaxy-Workflow-V1__Concatenate_reads_minion_files.ga
-workflow-install  -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb --publish_workflows -w Galaxy-Workflow-V1__Megablast_virus_prok_from_reads.ga
-workflow-install  -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb --publish_workflows -w Galaxy-Workflow-V2__Alignment___Get_unmapped_reads.ga
-workflow-install  -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb --publish_workflows -w Galaxy-Workflow-V3___alignment___assembly___megablast___tblastx.ga
-workflow-install  -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb --publish_workflows -w Galaxy-Workflow-V3___concatenate_megablast_results_if_needed.ga
+shed-tools install -g http://$ip  -a $api -t Galaxy-Workflow-Catch_clean_gi_to_DL_ref.yml
+shed-tools install -g http://$ip  -a $api -t Galaxy-Workflow-V1__Alignment___Variant_calling.yml
+shed-tools install -g http://$ip  -a $api -t Galaxy-Workflow-V1__Concatenate_reads_minion_files.yml
+shed-tools install -g http://$ip  -a $api -t Galaxy-Workflow-V1__Megablast_virus_prok_from_reads.yml
+shed-tools install -g http://$ip  -a $api -t Galaxy-Workflow-V2__Alignment___Get_unmapped_reads.yml
+shed-tools install -g http://$ip  -a $api -t Galaxy-Workflow-V3___alignment___assembly___megablast___tblastx.yml
+shed-tools install -g http://$ip  -a $api -t Galaxy-Workflow-V3___concatenate_megablast_results_if_needed.yml
+workflow-install  -g http://$ip  -a $api --publish_workflows -w Galaxy-Workflow-Catch_clean_gi_to_DL_ref.ga
+workflow-install  -g http://$ip  -a $api --publish_workflows -w Galaxy-Workflow-V1__Alignment___Variant_calling.ga
+workflow-install  -g http://$ip  -a $api --publish_workflows -w Galaxy-Workflow-V1__Concatenate_reads_minion_files.ga
+workflow-install  -g http://$ip  -a $api --publish_workflows -w Galaxy-Workflow-V1__Megablast_virus_prok_from_reads.ga
+workflow-install  -g http://$ip  -a $api --publish_workflows -w Galaxy-Workflow-V2__Alignment___Get_unmapped_reads.ga
+workflow-install  -g http://$ip  -a $api --publish_workflows -w Galaxy-Workflow-V3___alignment___assembly___megablast___tblastx.ga
+workflow-install  -g http://$ip  -a $api --publish_workflows -w Galaxy-Workflow-V3___concatenate_megablast_results_if_needed.ga
--- galaxy_workflow/install_2/workflow-and-tools-install.sh
+++ galaxy_workflow/install_2/workflow-and-tools-install.sh
@@ -1,9 +1,11 @@
+export ip=$1
+export api=$2
 workflow-to-tools -w Galaxy-Workflow-V1_BACT__Assembly___identification.ga  -o Galaxy-Workflow-V1_BACT__Assembly___identification.yml
 workflow-to-tools -w Galaxy-Workflow-V1_VIRAL__Assembly___identification.ga -o Galaxy-Workflow-V1_VIRAL__Assembly___identification.yml
 workflow-to-tools -w Galaxy-Workflow-V2__Host_caracterisation.ga            -o Galaxy-Workflow-V2__Host_caracterisation.yml
-shed-tools install -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb -t Galaxy-Workflow-V1_BACT__Assembly___identification.yml
-shed-tools install -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb -t Galaxy-Workflow-V1_VIRAL__Assembly___identification.yml
-shed-tools install -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb -t Galaxy-Workflow-V2__Host_caracterisation.yml
-workflow-install  -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb --publish_workflows -w Galaxy-Workflow-V1_BACT__Assembly___identification.ga
-workflow-install  -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb --publish_workflows -w Galaxy-Workflow-V1_VIRAL__Assembly___identification.ga
-workflow-install  -g http://192.168.101.87  -a 0356f1f8824d49d1c5815b14c976e9bb --publish_workflows -w Galaxy-Workflow-V2__Host_caracterisation.ga
+shed-tools install -g http://$ip  -a $api -t Galaxy-Workflow-V1_BACT__Assembly___identification.yml
+shed-tools install -g http://$ip  -a $api -t Galaxy-Workflow-V1_VIRAL__Assembly___identification.yml
+shed-tools install -g http://$ip  -a $api -t Galaxy-Workflow-V2__Host_caracterisation.yml
+workflow-install  -g http://$ip  -a $api --publish_workflows -w Galaxy-Workflow-V1_BACT__Assembly___identification.ga
+workflow-install  -g http://$ip  -a $api --publish_workflows -w Galaxy-Workflow-V1_VIRAL__Assembly___identification.ga
+workflow-install  -g http://$ip  -a $api --publish_workflows -w Galaxy-Workflow-V2__Host_caracterisation.ga
